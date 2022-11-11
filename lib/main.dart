b5929c9bdd977cf05320a6e973550827428de6c3
nandhakv12 <nandhakv12@gmail.com>
2022-11-11 16:03:16 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learn/view/login_view.dart';
 import 'package:learn/view/register_view.dart';
+import 'package:learn/view/verify_email.dart';
 
 import 'firebase_options.dart';
 
@@ -36,16 +37,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            //   //final user = FirebaseAuth.instance.currentUser;
-            //   // print(user);
-            //   // if (user?.emailVerified ?? false) {
-            //   // } else {
-            //   //   Navigator.of(context).push(MaterialPageRoute(
-            //   //       builder: (context) => const VerifyEmailView(),
-            //   //       ));
-            //   //   return const VerifyEmailView();
-            //   // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('email verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('done');
 
           default:
             return const CircularProgressIndicator();
