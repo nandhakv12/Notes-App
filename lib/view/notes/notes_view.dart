524fa04a0c47006067814912224b457223894d70
nandhakv12 <nandhakv12@gmail.com>
2022-11-20 11:43:45 +0530

    step-14

--- lib/view/notes/notes_view.dart
+++ lib/view/notes/notes_view.dart
@@ -78,6 +78,7 @@ class _NotesViewState extends State<NotesView> {
                 builder: ((context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
+                    case ConnectionState.active:
                       return const Text('waiting for all notes...');
 
                     default:
